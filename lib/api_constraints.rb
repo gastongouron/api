0818b2c61fe56d2f163708ef0e074cca976d2800
paulgastongouron <paul-gaston.gouron@paymium.com>
2018-01-04 16:24:56 +0100

    Updates application controller to prevent CSRF exception from being raised

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -5,6 +5,6 @@ class ApiConstraints
   end
 
   def matches?(req)
-    @default || req.headers['Accept'].include?("application/vnd.v#{@version}")
+    @default || req.headers['Accept'].include?("application/vnd.api.v#{@version}")
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.v1" }
+  before(:each) { request.headers['Accept'] = "application/vnd.api.v1" }
 
   describe "GET #show" do
     before(:each) do
@@ -9,7 +9,7 @@ describe Api::V1::UsersController do
       get :show, id: @user.id, format: :json
     end
 
-    it "returns the information about a reporter on a hash" do
+    it "returns the information about an user on a hash" do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
