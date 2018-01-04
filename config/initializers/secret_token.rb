f90f5edc86e2e8dde12adeeb660bdf4b7d2063d2
paulgastongouron <paul-gaston.gouron@paymium.com>
2018-01-04 15:29:39 +0100

    Initializes API project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Api::Application.config.secret_key_base = '6e78a725602a690f148ab134244d92aec441d4641ba058aad189a70ddec13e647de6c3e51976d8908dadfb546543fab5ef80c4fc73a56b60f6bcd7424a0a248d'
