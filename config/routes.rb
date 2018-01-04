ea64abb745ee61a35fa0e4da120ac076c864628f
paulgastongouron <paul-gaston.gouron@paymium.com>
2018-01-04 15:54:03 +0100

    Test API constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+require 'api_constraints'
+
 Api::Application.routes.draw do
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+
+    end
+
+  end
 
 end
