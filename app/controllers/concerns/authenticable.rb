b49bb9291ba464959bcd02d7bf4491a3c2019f40
paulgastongouron <paul-gaston.gouron@paymium.com>
2018-01-04 17:23:40 +0100

    Adds user_signed_in? method to know whether the user is logged in or not

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -4,6 +4,10 @@ module Authenticable
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
 
+  def user_signed_in?
+    current_user.present?
+  end
+
   def authenticate_with_token!
     render json: { errors: "Not authenticated" },
                    status: :unauthorized unless current_user.present?
